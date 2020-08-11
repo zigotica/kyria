ddbf0640b44e8ea83815b414098b79ebd912888a
zigotica <zigotica@gmail.com>
2020-08-11 18:03:09 +0200

    shift

--- encoder.c
+++ encoder.c
@@ -2,8 +2,8 @@
 
 void encoder_update_user(uint8_t index, bool clockwise)
 {
-  // check if ctrl is pressed
-  const uint16_t ctron = keyboard_report->mods & (MOD_BIT(KC_LCTL));
+  // check if shift is pressed
+  const uint16_t shift_on = keyboard_report->mods & (MOD_BIT(KC_LSFT));
 
   if (index == 0) {
     // Volume control
@@ -14,15 +14,15 @@ void encoder_update_user(uint8_t index, bool clockwise)
     }
   } else if (index == 1) {
     // Arrows up/down (scroll, move line, ...)
-    if (ctron) {
-      unregister_code(KC_LCTL);
+    if (shift_on) {
+      unregister_code(KC_LSFT);
       if (clockwise) {
         tap_code(KC_RIGHT);
       } else {
         tap_code(KC_LEFT);
       }
       // reattach the mod so we can keep rotating
-      register_code(KC_LCTL);
+      register_code(KC_LSFT);
     } else {
       if (clockwise) {
         tap_code(KC_DOWN);
